998e1745e5c338b047332261470f7be5d5739e4e
alice <dothinh.dev@gmail.com>
2023-11-18 16:18:11 +0700

    gw command

--- discord-bot/my-bot.py
+++ discord-bot/my-bot.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import discord
 import os
 from dotenv import load_dotenv
@@ -6,8 +7,6 @@ import sys
 sys.path.append(r'D:\project\footystats')
 from server.api import static
 
-from datetime import datetime
-
 load_dotenv()
 
 intents = discord.Intents.default()
@@ -15,18 +14,22 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$gw1'):
+    if message.content.startswith('$dl'):
         live = static()
         events = live.get_events()
-        await message.channel.send(f'Deadline time: {datetime.fromisoformat(events.iloc[0][2]).date()}')
+        gwid = message.content[3:]
+        deadline = datetime.fromisoformat(events.iloc[int(gwid)-1][2]).strftime('%I:%M%p %b %d %Y')
+        await message.channel.send(f'Deadline time for gameweek {gwid}: {deadline}.')
 
 client.run(os.environ.get('TOKEN'))
